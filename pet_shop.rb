24e6404f23fe09645cc212f3b4da318e92de63bd
Suzanne <suzanne.aitchison@gmail.com>
2017-02-03 14:35:37 +0000

    refactored sell_pet_to_customer to make code more DRY

--- pet_shop.rb
+++ pet_shop.rb
@@ -98,8 +98,8 @@ end
 
 def sell_pet_to_customer(pet_shop, pet, customer)
 
-  customer[:pets] << pet
-  pet_shop[:admin][:pets_sold] += 1
-  pet_shop[:admin][:total_cash] += pet[:price]
+  add_pet_to_customer(customer, pet)
+  increase_pets_sold(pet_shop, 1)
+  add_or_remove_cash(pet_shop, pet[:price])
 
 end
